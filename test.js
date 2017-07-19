295cb68dbb841ddcaaed150e9e29ce3491eca956
shenlieli <shenll@getui.com>
2017-07-19 20:50:26 +0800

    删除注释

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ const _ = require('lodash');
 
 
 // create
-/*  co(function* () {
+co(function* () {
     let arrFields = { firstName: 'John ', lastName: 'senior engineer'};
     let result = yield User.create(arrFields, {'raw': true});
     // console.log(result);
@@ -12,7 +12,7 @@ const _ = require('lodash');
     console.log(result.dataValues);
     return result;
     // console.log(result.get({'plain': true})); // 获得干净的对象，如save、update、destroy
-});  */ 
+}); 
  
 // batch create
 co(function* () {
@@ -30,40 +30,40 @@ co(function* () {
 })   
  
 // update
-/* co(function* () {
+co(function* () {
     let arrFields = { firstName: "小白"};
     let result = yield User.update(arrFields, { where: {id: 2} }) // 返回更新的条数
     if (result[0] === 1) {
         console.log("success");
     }
     console.log(result);
-})  */
+})  
 
 // delete
-/* co(function* () {
+co(function* () {
     let result = yield User.destroy({ where: {id: 4} }); // 返回删除的条数
     console.log(result);
-}) */
+}) 
 
 // batch delete 
-/* co(function* () {
+co(function* () {
     let result = yield User.destroy( { where: { id: [4, 5, 6] } })
     console.log(result)
-}) */
+}) 
 
 // findOne
-/* co(function* () {
+co(function* () {
     let where = {id: 2}
     let result2 = yield User.findOne({"where": where, raw: true}); // raw:true可解决产生dataValues的问题
     console.log(result2);
-}); */
+}); 
 
 // findAll
-/* co(function* () {
+co(function* () {
     let result = yield User.findAll({raw: true});
     console.log(result);
 })
- */
+ 
 
 // 多表 
 
